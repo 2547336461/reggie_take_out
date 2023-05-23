fe104d069d9bcbe8d3684a98d8286e0b746f7a56
taodada <2547336461@qq.com>
2023-05-23 13:11:18 +0800

    文件上传下载

--- src/main/java/com/itheima/reggie/controller/CommonController.java
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -3,12 +3,14 @@ package com.itheima.reggie.controller;
 import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.UUID;
 
 /**
@@ -45,20 +47,51 @@ public class CommonController {
 
         // 防止目录不存在，加个判断
         File dir = new File(basePath);
-        if (!dir.exists()){
+        if (!dir.exists()) {
             // 目录不存在，需要创建
             dir.mkdirs();
         }
 
         try {
             //将临时文件转存到指定位置(通过配置文件更改位置)
-            file.transferTo(new File(basePath+fileName));
+            file.transferTo(new File(basePath + fileName));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return R.success(fileName);
     }
 
+    /**
+     * 文件下载
+     *
+     * @param name
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+        try {
+            // 输入流，通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+
+            // 输出流，通过输出流将文件写回浏览器，在浏览器展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            // 设置响应格式
+            response.setContentType("image/jpg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
+                outputStream.flush();
+            }
+            // 关闭资源
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }

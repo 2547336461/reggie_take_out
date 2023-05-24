745c7af9c90b9ec2c0325d9e0166913367415fe3
taodada <2547336461@qq.com>
2023-05-24 11:28:49 +0800

    批量删除套餐

--- src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -1,14 +1,23 @@
 package com.itheima.reggie.controller;
 
+import com.alibaba.druid.util.Utils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
+import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.SetmealDishService;
 import com.itheima.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 套餐管理
@@ -23,6 +32,10 @@ public class SetmealController {
 
     @Autowired
     private SetmealService setmealService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private SetmealDishService setmealDishService;
 
     /**
      * 新增套餐
@@ -35,5 +48,55 @@ public class SetmealController {
         log.info("套餐信息为：{}",setmealDto);
         setmealService.saveWithDish(setmealDto);
         return R.success("新增套餐成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        // 构造分页构造器对象
+        Page<Setmeal> pageInfo = new Page<>(page,pageSize);
+        // 构造一个新的分页构造器用于满足前端要求插入一个categoryName属性
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加查询条件
+        queryWrapper.like(name!= null,Setmeal::getName,name);
+        // 排序条件
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+        setmealService.page(pageInfo,queryWrapper);
+
+        // 执行完上面方法后将pageInfo属性拷贝到seteamlDtoPage
+        BeanUtils.copyProperties(pageInfo,setmealDtoPage,"records");
+        List<Setmeal> records = pageInfo.getRecords();
+        // 处理集合
+        List<SetmealDto> list = records.stream().map((item) -> {
+            // 创建一个新的setmealDto用于保存
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(item, setmealDto); // 拷贝
+            Long categoryId = item.getCategoryId();     // 分类id
+            Category category = categoryService.getById(categoryId);
+            if (category!= null){
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        // 修改完成后导入数据
+        setmealDtoPage.setRecords(list);
+        return R.success(setmealDtoPage);
+    }
+
+    /**
+     * 删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids){
+        log.info("ids:{}",ids);
+        setmealService.deleteWithDish(ids);
+        return R.success("套餐数据删除成功");
     }
 }
--- src/main/java/com/itheima/reggie/service/SetmealService.java
+++ src/main/java/com/itheima/reggie/service/SetmealService.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.reggie.dto.SetmealDto;
 import com.itheima.reggie.entity.Setmeal;
 
+import java.util.List;
+
 /**
  * @Author Tao
  * @Date 2023 05 22 19 41
@@ -11,4 +13,7 @@ import com.itheima.reggie.entity.Setmeal;
 public interface SetmealService extends IService<Setmeal> {
     // 新增套餐，同时需要保存套餐和菜品的关联关系
     public void saveWithDish(SetmealDto setmealDto);
+
+    // 删除套餐，同时需要删除套餐和菜品的关联关系
+    public void deleteWithDish(List<Long> ids);
 }

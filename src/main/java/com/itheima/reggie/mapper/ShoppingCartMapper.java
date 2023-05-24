85ccb21b5bff6a0e7192407e74620c89414b71fd
taodada <2547336461@qq.com>
2023-05-24 22:23:38 +0800

    下订单，以及完善

---
+++ src/main/java/com/itheima/reggie/mapper/ShoppingCartMapper.java
@@ -0,0 +1,13 @@
+package com.itheima.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.reggie.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author Tao
+ * @Date 2023 05 24 18 39
+ **/
+@Mapper
+public interface ShoppingCartMapper extends BaseMapper<ShoppingCart> {
+}
---
+++ src/main/java/com/itheima/reggie/service/ShoppingCartService.java
@@ -0,0 +1,11 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.entity.ShoppingCart;
+
+/**
+ * @Author Tao
+ * @Date 2023 05 24 18 40
+ **/
+public interface ShoppingCartService extends IService<ShoppingCart> {
+}
